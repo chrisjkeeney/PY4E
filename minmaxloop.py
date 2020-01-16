474e033969ea45ffc7d2e32f13e7a5c62d9ae5b8
Chris Keeney <chrisjkeeney@gmail.com>
2020-01-15 22:28:50 -0600

    5.2

---
+++ minmaxloop.py
@@ -0,0 +1,17 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+        if smallest is None or smallest > num:
+            smallest = num
+        if largest is None or largest < num:
+            largest = num
+    except:
+        print('Invalid input')
+        continue
+    print("Maximum is", largest)
+    print("Minimum is", smallest)
